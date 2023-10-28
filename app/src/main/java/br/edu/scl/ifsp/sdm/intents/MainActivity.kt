710ee1e489267d2ff8aa21f00eb433355a232fd0
Lucas de Oliveira Macedo <lucasomac@outlook.com>
2023-10-27 21:48:03 -0300

    Initial commit

---
+++ app/src/main/java/br/edu/scl/ifsp/sdm/intents/MainActivity.kt
@@ -0,0 +1,100 @@
+package br.edu.scl.ifsp.sdm.intents
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import br.edu.scl.ifsp.sdm.intents.Extras.PARAMETER_EXTRA
+import br.edu.scl.ifsp.sdm.intents.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private val activityMainBinding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    //Usando o lazzy garanto que ele sera inicializado apenas qunado necessario
+    //Na aula foi usado o lateinit var e inicializado no onCreate
+    private val parameterResultLauncher: ActivityResultLauncher<Intent> by lazy {
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == RESULT_OK) {
+                result?.data?.getStringExtra(PARAMETER_EXTRA).also {
+                    activityMainBinding.parameterTv.text = it
+                }
+            }
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(activityMainBinding.root)
+        setSupportActionBar(activityMainBinding.toolbarIn.toolbar)
+        supportActionBar?.subtitle = localClassName
+
+        activityMainBinding.apply {
+            parameterBt.setOnClickListener {
+                val parameterIntent =
+                    Intent(this@MainActivity, ParameterActivity::class.java).apply {
+                        putExtra(PARAMETER_EXTRA, parameterTv.text.toString())
+                    }
+                //Forma depreciada
+                //startActivityForResult(parameterIntent, PARAMETER_REQUEST_CODE)
+                parameterResultLauncher.launch(parameterIntent)
+            }
+        }
+    }
+    // Forma depreciada
+    //    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+    //        super.onActivityResult(requestCode, resultCode, data)
+    //        if (requestCode == 0 && resultCode == RESULT_OK) {
+    //            data?.getStringExtra(PARAMETER_EXTRA).also {
+    //                activityMainBinding.parameterTv.text = it
+    //            }
+    //        }
+    //    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.openActivityMi -> {
+
+                true
+            }
+
+            R.id.viewMi -> {
+                true
+            }
+
+            R.id.callMi -> {
+                true
+            }
+
+            R.id.dialMi -> {
+                true
+            }
+
+            R.id.pickMi -> {
+                true
+            }
+
+            R.id.chooserMi -> {
+                true
+            }
+
+            else -> {
+                false
+            }
+        }
+    }
+
+    companion object {
+        // Forma depreciada
+//        private const val PARAMETER_REQUEST_CODE = 0
+    }
+}
